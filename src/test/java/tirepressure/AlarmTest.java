f2b8df7753fb2128c2d96eeab3a0221a3ae985ff
Martin Blomberg <martin.blomberg@outlook.com>
2021-03-26 09:58:36 +0100

    Pressure outside interval

--- src/test/java/tirepressure/AlarmTest.java
+++ src/test/java/tirepressure/AlarmTest.java
@@ -19,4 +19,28 @@ class AlarmTest {
 
         assertThat(alarm.isAlarmOn()).isFalse();
     }
+
+    @Test
+    void pressureIsLow() {
+        Sensor sensor = mock(Sensor.class);
+        when(sensor.popNextPressurePsiValue()).thenReturn(16.0);
+        Alarm alarm = new Alarm(sensor, new Threshold(17, 21));
+
+        alarm.check();
+
+        assertThat(alarm.isAlarmOn()).isTrue();
+    }
+
+    @Test
+    void pressureIsHigh() {
+        Sensor sensor = mock(Sensor.class);
+        when(sensor.popNextPressurePsiValue()).thenReturn(22.0);
+        Alarm alarm = new Alarm(sensor, new Threshold(17.0, 21.0));
+
+        alarm.check();
+
+        assertThat(alarm.isAlarmOn()).isTrue();
+    }
+
+
 }
