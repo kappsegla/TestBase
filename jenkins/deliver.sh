19278f3bf97d9cd01932851b58b11d16728ea477
Martin Blomberg <martin.blomberg@outlook.com>
2021-03-27 20:06:16 +0100

    Updated deliver.sh

--- jenkins/deliver.sh
+++ jenkins/deliver.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
-
-#echo 'The following Maven command installs your Maven-built Java application'
-#echo 'into the local Maven repository, which will ultimately be stored in'
-#echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
-#echo 'volume).'
-#set -x
-#mvn jar:jar install:install help:evaluate -Dexpression=project.name
-#set +x
-
 echo 'The following complex command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
@@ -23,4 +14,4 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar target/${NAME}-${VERSION}.jar
+java -jar target/${NAME}-${VERSION}.jar mtk.CompanyRunner
